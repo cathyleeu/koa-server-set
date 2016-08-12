00f0d3589ac0731382b2ce06aa34da470c60b1e0
cathyleeu <leeyu823@gmail.com>
2016-08-12 13:07:10 +0900

    mongoose

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('babel-polyfill');
 const config = require('./src/config');
 const app = require('./src/server');
 
+
 // http://www.marcusoft.net/2015/10/eaddrinuse-when-watching-tests-with-mocha-and-supertest.html
 // Check for module parent and export app, otherwise app.listen()
 // This is so that it is easy to test application or use it with clustering
--- src/api/root/root.controller.js
+++ src/api/root/root.controller.js
@@ -36,6 +36,4 @@ function* index(ctx, next) {
 }
 
 
-
-
 */

f1d78e239e84a7ae8c1daab82bd1744f59dc0ef4
cathyleeu <leeyu823@gmail.com>
2016-08-15 21:02:39 +0900

    test

--- src/api/root/index.js
+++ src/api/root/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
-import { index, post} from './root.controller';
+import { index, feed} from './root.controller';
 import router from 'koa-router';
 
 
 const root = router();
 
 root.get('/', index);
-root.get('/post', post);
+root.get('/feed', feed);
 
 export default root;
+
+
+//root.get('./(http에 적혀질 주소), controller에 있는 주소')
--- src/api/root/root.model.js
+++ src/api/root/root.model.js
@@ -2,30 +2,28 @@
 
 import config from '../../config';
 import mongoose from 'mongoose';
+import post from './model/post'
 
 export function list() {
   return new Promise((resolve) => {
     setTimeout(() => { //  resolve(config) resolve일종의 return
       // // 의미 있는거 아녕
-      var kittySchema = mongoose.Schema({
-          name: String
-      });
-      var Kitten = mongoose.model('cats', kittySchema);
-      var res = Kitten.find({ name: 'Zildjian' });
+      // var kittySchema = mongoose.Schema({
+      //     name: String
+      // });
+      // var Kitten = mongoose.model('cats', kittySchema);
+      // var res = Kitten.find({ name: 'Zildjian' });
       // var zizi = mongoose.get('Cat').find({'name':'Zildjian'});
-      resolve(res);
+      resolve(config);
     });
   });
 }
 
-export function list2() {
+export function post() {
   return new Promise((resolve) => {
     setTimeout(() => { //  resolve(config) resolve일종의 return
       // 의미 있는거 아녕~
-      resolve({
-        a: 1,
-        b: 2
-      });
+      resolve(post);
     });
   });
 }

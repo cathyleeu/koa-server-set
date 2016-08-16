296637960a26d9fe48ff425bf4440e38460f2f93
cathyleeu <leeyu823@gmail.com>
2016-08-16 18:57:44 +0900

    no

--- src/config/env/common.js
+++ src/config/env/common.js
@@ -3,6 +3,6 @@ const path = require('path');
 export default {
   port: process.env.PORT || 9000,
   ip: process.env.IP || 'localhost',
-  root: path.normalize(__dirname + '../../../'),
+  root: path.normalize(__dirname + '../../'),
   logType: process.env.LOGTYPE || 'dev',
 }
